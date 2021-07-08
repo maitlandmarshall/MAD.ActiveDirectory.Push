f2bc162c72b389b6e78eb8823c503c827d1220f0
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-07-08 10:35:23 +1000

    Add project files.

---
+++ MAD.ActiveDirectory.Push/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MAD.ActiveDirectory.Push
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
